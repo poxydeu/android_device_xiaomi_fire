f3ea88aabff0b246b763df00b341c6b28d006e4b
Giovanni Ricca <giovanniricca@proton.me>
2026-02-02 10:17:53 +0100

    fire: Inherit common Mediatek FCM
    Change-Id: I2807b7f2b0c86305fea56f28f0137ea2166ebade

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,7 +86,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/framework_compatibility_matrix.xml \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
 # Kernel
